e284d3a5735a52c6e1b0ea0ff9985ebc26260a3e
jozsefrado <jozsef.rado@ibm.com>
2021-03-11 22:48:48 +0100

    blabal

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_h!&vqh!6r797iexatik$l#e6+uqx_k$1q+w#imneg3lqj3j^v'
+SECRET_KEY = 'NICK_h!&vqh!6r797iexatik$l#e6+uqx_k$1q+w#imneg3lqj3j^v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
